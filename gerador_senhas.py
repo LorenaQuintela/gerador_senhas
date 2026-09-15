865caab830faa410581b907f452be46ff4f6084e
Bowie <lorenalrn22@gmail.com>
2026-09-15 14:41:09 -0400

    Variáveis

--- gerador_senhas.py
+++ gerador_senhas.py
@@ -1,1 +1,11 @@
-print()
+from random import random
+
+lista_letras = []
+lista_numeros = []
+lista_simbolos = []
+
+lista_aleatória = []
+qtd_letras = input("Quantas letras você quer na senha? ")
+qtd_numeros = input("Quantos números você quer na senha? ")
+qtd_simbolos = input("Quantos simbolos? ")
+senha_gerada = random(qtd_letras, qtd_numeros, qtd_simbolos)
