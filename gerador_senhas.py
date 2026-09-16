7ab84d2ed155615517177753f1a722f3fae6e497
Bowie <lorenalrn22@gmail.com>
2026-09-16 14:49:14 -0400

    Implementando o primeiro for

--- gerador_senhas.py
+++ gerador_senhas.py
@@ -1,12 +1,22 @@
-from random import random
+from random import choice, shuffle
 
-lista_letras = []
-lista_numeros = []
-lista_simbolos = []
+lista_letras = ["a", "b", "c", "d", "e",
+    "f", "g", "h", "i", "j",
+    "k", "l", "m", "n", "o",
+    "p", "q", "r", "s", "t",
+    "u", "v", "w", "x", "y",
+    "z"]
+lista_numeros = ["0", "1", "2", "3", "4",
+    "5", "6", "7", "8", "9"]
+lista_simbolos = ["!", "@", "#", "$", "%"]
+
+qtd_letras = int(input("Quantas letras você quer na senha? "))
+qtd_numeros = int(input("Quantos números você quer na senha? "))
+qtd_simbolos = int(input("Quantos simbolos? "))
+
 
 lista_aleatória = []
 
-qtd_letras = input("Quantas letras você quer na senha? ")
-qtd_numeros = input("Quantos números você quer na senha? ")
-qtd_simbolos = input("Quantos simbolos? ")
-senha_gerada = random(qtd_letras, qtd_numeros, qtd_simbolos)
+for i in range(qtd_letras):
+  letra_aleatoria = choice(lista_letras)
+  lista_aleatória.append(letra_aleatoria)
